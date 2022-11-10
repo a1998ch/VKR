ad6ab90ff35918f05dddc493308ae29ed91089e3
a1998ch <a1998ch@gmail.com>
2022-11-10 16:07:59 +0700

    Внесены изменения

--- VKR/MethodologyModel/PowerReserve.cs
+++ VKR/MethodologyModel/PowerReserve.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using ExcelModel;
+//using ExcelModel;
 using System.IO;
 
 namespace MethodologyModel
